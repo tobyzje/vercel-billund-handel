5fae53f3a0ce3dd9dce42049bc16c6056b2c3fa4
tobyzje <tstoklund9@gmail.com>
2024-12-01 22:44:28 +0100

    Commit 3

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
+  },
   server: {
     port: 5173,
     strictPort: false,
-    host: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false
-      }
-    },
-    hmr: {
-      overlay: true,
-      clientPort: 5173
-    }
+    host: true
   }
 })
