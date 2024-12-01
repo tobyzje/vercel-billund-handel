946fed48f6423e1a14bc07a53f240bc227e07d63
tobyzje <tstoklund9@gmail.com>
2024-12-01 21:13:44 +0100

    MongoDB Connected

--- server/server.js
+++ server/server.js
@@ -10,8 +10,15 @@ const app = express()
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
-  credentials: true
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'https://billund-handelsforening.vercel.app',
+    /\.vercel\.app$/
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }))
 app.use(express.json())
 app.use(cookieParser())
